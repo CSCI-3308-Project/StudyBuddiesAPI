87dd95e4f47a2c477349feedb53131177b8c4194
Justin Adkins <jadkins1019@gmail.com>
2018-12-10 00:57:59 -0700

    Updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: 'postgres://qberjqfjwamabb:12ced311acc0ac029cca5121e952abb4bd8b371510b42eaf4babbee9ce319d3f@ec2-184-72-239-186.compute-1.amazonaws.com:5432/d3lcoi1564acr9',
     migrations: {
       directory: __dirname + '/db/migrations',
     },
